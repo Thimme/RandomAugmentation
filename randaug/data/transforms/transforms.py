fix rotation transform memory leak and transforms sampler

randaug/data/transforms/transforms.py
@@ -2,6 +2,7 @@ import numpy as np
 import albumentations as A
 import os
 import math
+import random
 from detectron2.utils import comm
 from PIL import ImageOps, ImageEnhance
 from detectron2.data import transforms as T
@@ -44,6 +45,11 @@ class Augmentations(Enum):
     SHARPNESS = 'sharpness'
     CUTOUT = 'cutout'
 
+def random_invert(number):
+    if random.choice([True, False]):
+        return -number
+    else:
+        return number
 
 # Own augmentations
 class FogAugmentation(T.Augmentation):
@@ -253,7 +259,8 @@ class ShearXAugmentation(T.Augmentation):
         self.magnitude = magnitude
         
     def get_transform(self, image):
-        v = np.linspace(-20, 20, num=MAGNITUDE_BINS) # Shear in degrees 
+        v = np.linspace(5, 25, num=MAGNITUDE_BINS) # Shear in degrees 
+        v = random_invert(number=v)
         transform = A.Affine(shear={'x': v[self.magnitude], 'y': 0})
         params = prepare_param(transform, image)
         return AlbumentationsTransform(transform, params, size=(image.shape[:2]))
@@ -267,7 +274,8 @@ class ShearYAugmentation(T.Augmentation):
         self.magnitude = magnitude
         
     def get_transform(self, image):
-        v = np.linspace(-20, 20, num=MAGNITUDE_BINS) # Shear in degrees 
+        v = np.linspace(5, 25, num=MAGNITUDE_BINS) # Shear in degrees 
+        v = random_invert(number=v)
         transform = A.Affine(shear={'x': 0, 'y': v[self.magnitude]})
         params = prepare_param(transform, image)
         return AlbumentationsTransform(transform, params, size=(image.shape[:2]))
@@ -281,7 +289,8 @@ class TranslateXAugmentation(T.Augmentation):
         self.magnitude = magnitude
         
     def get_transform(self, image): 
-        v = np.linspace(-0.45, 0.45, num=MAGNITUDE_BINS)
+        v = np.linspace(0.1, 0.45, num=MAGNITUDE_BINS)
+        v = random_invert(number=v)
         transform = A.Affine(translate_percent={'x': v[self.magnitude], 'y': 0})
         params = prepare_param(transform, image)
         return AlbumentationsTransform(transform, params, size=(image.shape[:2]))
@@ -295,7 +304,8 @@ class TranslateYAugmentation(T.Augmentation):
         self.magnitude = magnitude
         
     def get_transform(self, image):
-        v = np.linspace(-0.45, 0.45, num=MAGNITUDE_BINS)
+        v = np.linspace(0.1, 0.45, num=MAGNITUDE_BINS)
+        v = random_invert(number=v)
         transform = A.Affine(translate_percent={'x': 0, 'y': v[self.magnitude]})
         params = prepare_param(transform, image)
         return AlbumentationsTransform(transform, params, size=(image.shape[:2]))  
@@ -310,9 +320,12 @@ class RotationAugmentation(T.Augmentation):
         self.magnitude = magnitude
         
     def get_transform(self, image):
-        h, w = image.shape[:2]
-        angle = np.linspace(-30, 30, num=MAGNITUDE_BINS)
-        return T.RotationTransform(h, w, angle[self.magnitude])
+        angle = np.linspace(5, 30, num=MAGNITUDE_BINS)
+        angle = random_invert(number=angle)
+        transform = A.Affine(rotate=angle[self.magnitude])
+        params = prepare_param(transform, image)
+        return AlbumentationsTransform(transform, params, size=(image.shape[:2]))  
+        #return T.RotationTransform(h, w, angle[self.magnitude])
     
 
 class AutoContrastAugmentation(T.Augmentation):

randaug/engine/transform_sampler.py
@@ -7,20 +7,35 @@ from randaug.data.transforms.transforms import *
 
 from itertools import product
 
-
-gan_transforms = [ 
+gan_transforms = [
     "CycleGANFogAugmentation",
     "CycleGANRainAugmentation",
     "CycleGANSnowAugmentation",
     "CUTFogAugmentation",
     "CUTRainAugmentation",
     "CUTSnowAugmentation",
+]
+
+diffusion_transforms = [
     "StableDiffusionFogAugmentation",
     "StableDiffusionRainAugmentation",
     "StableDiffusionSnowAugmentation",
     "CycleDiffusionFogAugmentation",
     "CycleDiffusionRainAugmentation",
-    "CycleDiffusionSnowAugmentation"
+    "CycleDiffusionSnowAugmentation",
+]
+
+ai_transforms = [
+    "CycleGAN",
+    "CUT",
+    "StableDiffusion",
+    "CycleDiffusion",
+]
+
+ai_conditions = [
+    "FogAugmentation",
+    "RainAugmentation",
+    "SnowAugmentation",
 ]
 
 image_transforms = [
@@ -32,10 +47,9 @@ image_transforms = [
     "RainAugmentation",
     "TranslateXAugmentation",
     "TranslateYAugmentation",
-    "RotationAugmentation",
     "AutoContrastAugmentation",
     "InvertAugmentation",
-    "EqualizeAugmentation",
+    "EqualizeAugmentation", #
     "SolarizeAugmentation",
     "PosterizeAugmentation",
     "ContrastAugmentation",
@@ -43,6 +57,7 @@ image_transforms = [
     "SharpnessAugmentation",
     "CutoutAugmentation",
     "DropAugmentation",
+    "RotationAugmentation",
 ]
 
 NUM_DATASETS = 32
@@ -55,7 +70,7 @@ class TransformSampler():
         np.random.seed(42) # random seed is required for parallelisation to have same magnitudes on every process
 
     def _sample_transforms(self, N):
-        return [t for t in product(gan_transforms + image_transforms, repeat = N)]
+        return [t for t in product(gan_transforms + diffusion_transforms + image_transforms, repeat = N)]
         
     def _map_to_transforms(self, ops):
         transforms = [getattr(sys.modules[__name__], op[0]) for op in ops]
@@ -90,7 +105,7 @@ class TransformSampler():
         filtered = []
         for op in ops:
             second_aug = op[1][0]
-            if second_aug not in gan_transforms:
+            if second_aug not in gan_transforms + diffusion_transforms:
                 filtered.append(op)
         return filtered
 
@@ -135,7 +150,7 @@ class TransformSampler():
         return [RandomAugmentation(self.cfg, 1, [])]
     
     def test(self):
-        return [RandomAugmentation(self.cfg, 1, [DropAugmentation(magnitude=4), ColorAugmentation(magnitude=4)])]
+        return [RandomAugmentation(self.cfg, 1, [ShearYAugmentation(magnitude=4), ColorAugmentation(magnitude=4)])]
     
     def sample_output(self, magnitude=0):
         # amount of images
@@ -158,8 +173,8 @@ class RandomSampler():
         self.device = device
 
     def _sample_ops(self, N):
-        transforms = gan_transforms + image_transforms
-        one_time = gan_transforms # ["DropAugmentation"]
+        transforms = ai_transforms + image_transforms
+        one_time = ai_transforms # ["DropAugmentation"]
 
         while True:
             # Sample elements from A
@@ -169,7 +184,15 @@ class RandomSampler():
 
             # It does not make sense to have more than one gan / diffusion transform as it replaces the rest of the augmentations
             if gan_count <= 1:
+                sampled = [self._replace_sample(s) for s in sampled]
                 return sampled
+            
+    def _replace_sample(self, sample):
+        if sample in ai_transforms:
+            condition = random.choice(ai_conditions)
+            return f"{sample}{condition}"
+        else:
+            return sample
     
     def _add_magnitude(self, ops, M):
         magnitude = M
@@ -199,7 +222,7 @@ class RandomSampler():
 
     def random_augmentation(self, N, M):
         ops = self._sample_ops(N)
-        ops = self._reorder_ops(ops, gan_transforms)
+        ops = self._reorder_ops(ops, gan_transforms + diffusion_transforms)
         ops = self._add_magnitude(ops, M)
         transforms, _ = self._map_to_transforms(ops)
         return transforms